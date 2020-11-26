3fad34916b66988172f1cf1bf70408868312b04b
최성익 <sungik.dev@gmail.com>
2020-11-26 18:19:12 +0900

    Feat : 임시 서버 데이터 패칭 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 
 const App = (): JSX.Element => {
-  return <Div></Div>;
-};
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        "https://restcountries.eu/rest/v2/all?fields=alpha2Code;capital;name;region;callingCodes",
+      );
+      const json = await res.json();
+      console.log(json);
+      setData(json);
+    };
 
-const Div = styled.div``;
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data.map(({ name }) => (
+        <li>{name}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default App;
