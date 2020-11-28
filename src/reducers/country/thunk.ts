f30941b2d358eda0c62de7c48e9f735515417433
최성익 <sungik.dev@gmail.com>
2020-11-28 22:12:58 +0900

    Feat : thunk 코드 타입 명시

--- src/reducers/country/thunk.ts
+++ src/reducers/country/thunk.ts
@@ -1,14 +1,14 @@
 import { getCountryDataRequest, getCountryDataSuccess, getCountryDataFailure } from "./actions";
 import { v4 as uuidv4 } from "uuid";
-import { ICountryExceptId } from "./types";
+import { ICountry, ICountryExceptId } from "./types";
 import { AnyAction, Dispatch } from "redux";
 
 export const getCountryList = async (dispatch: Dispatch<AnyAction>): Promise<void> => {
   dispatch(getCountryDataRequest());
   try {
     const response = await fetch(process.env.API_URL);
-    const json = await response.json();
-    const data = json.map((eachData: ICountryExceptId) => ({ id: uuidv4(), ...eachData }));
+    const json: ICountryExceptId[] = await response.json();
+    const data: ICountry[] = json.map((eachData) => ({ id: uuidv4(), ...eachData }));
 
     dispatch(getCountryDataSuccess(data));
   } catch (e) {
